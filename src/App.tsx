bd62178e02655ff57c334c13b7a565ef05df7fa5
domingasp <d.pleck@outlook.com>
2022-08-03 07:53:24 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>Vite + React Starter</h1>
+      <p>The following have been configured:</p>
+      <ul>
+        <li>ESLint</li>
+        <li>Prettier</li>
+        <li>ESLint AirBnb Style Guide</li>
+        <li>VSCode Settings</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
